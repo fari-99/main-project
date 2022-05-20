499109eb931927b8687ebcdf8dd3a4ae9e37c7ed
Fadhlan Rizal <fadhlan.fari@gmail.com>
2022-05-20 13:09:22 +0700

    updates

--- servers/proxy/startup.sh
+++ servers/proxy/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+source services.sh
+
 # Add php-fpm as command if needed
 if [ "${1:0:1}" = '-' ]; then
 	set -- nginx "$@"
@@ -11,32 +13,24 @@ FILES=/nginx_templates/*.conf
 
 if [ ! -d /etc/nginx/sites-available/ ]; then
 	mkdir -p /etc/nginx/sites-available/
+else
+	# Empty all the available sites every time restart to make sure
+	# keep updated with SVC_LISTS value
+	rm /etc/nginx/sites-available/*.conf
 fi
 
-for file in $FILES
-do
-    filename="${file##*/}"
-
-    if [ ! -f /etc/nginx/sites-available/$filename ]; then
-    	case $filename in
-            "sentry.conf")
-                if [ $ENABLE_SENTRY == "false" ]; then
-                    proceed="false"
-                fi
-                ;;
-    		*)
-				proceed="true"
-    			;;
-    	esac
-		    	
-    	if [ $proceed == "true" ]; then
-	    	echo "Processing $file file..."
-	    	j2 $file > /etc/nginx/sites-available/$filename
-	    fi
-    else
-    	echo "$file exists or not required, skipping..."
-    fi
-done
+# Get apps from environment
+CONTAINER_LISTS+=""
+while IFS=' ' read -ra APPS; do
+	for i in "${APPS[@]}"; do
+		if [ -f /nginx_templates/$i.conf ]; then
+			echo "Processing $i service..."
+			j2 /nginx_templates/$i.conf > /etc/nginx/sites-available/$i.conf
+		else
+			echo "$i template is not found, skip..."
+		fi
+	done
+done <<< "$CONTAINER_LISTS"
 
 echo "[Finish]"
 
