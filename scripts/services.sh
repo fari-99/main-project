a244d9943a1484c0f25bcf3ae8ba6894f7869422
Fadhlan Rizal <fadhlan.fari@gmail.com>
2025-03-08 22:06:58 +0700

    proxy manager

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 applications=()
-SUPPORT_CONTAINERS='proxy mailcatcher redis redis-management elasticsearch kibana rabbitmq sftp'
+SUPPORT_CONTAINERS='proxy mailcatcher redis redis-management elasticsearch rabbitmq sftp'
 # MIGRATION_CONTAINERS='db-migration' # UNCOMMENT THIS FOR FIRST TIME SETUP
 DATABASE_CONTAINERS='db-web db-postgres'
 DESIGN_CONTAINERS=''
